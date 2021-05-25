383aa7c7e4558e5b119d8998f13bd7202949a9cb
buhari44 <adeyemibuari@gmail.com>
2021-05-25 18:13:59 +0100

    Done with the home page and the responsiveness

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("menu-icon");
+const NavMenu = document.querySelector(".nav-items");
+const main = document.querySelector(".all-part");
+const imageOverlay = document.querySelector(".img-overlay");
+function toggleNavbar() {
+  NavMenu.classList.toggle("open-nav");
+  main.classList.toggle("main-none");
+  imageOverlay.style.height = "100vh";
+}
