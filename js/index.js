e5951350d06facdea4bdb4cbb1a1f584bf7bcd57
buhari44 <adeyemibuari@gmail.com>
2021-05-25 23:16:00 +0100

    responsivenes of the navbar is breaking so it is fixed already

--- js/index.js
+++ js/index.js
@@ -2,8 +2,11 @@ const menu = document.getElementById("menu-icon");
 const NavMenu = document.querySelector(".nav-items");
 const main = document.querySelector(".all-part");
 const imageOverlay = document.querySelector(".img-overlay");
+const changeName = document.getElementById("change-name");
 function toggleNavbar() {
   NavMenu.classList.toggle("open-nav");
   main.classList.toggle("main-none");
   imageOverlay.style.height = "100vh";
+  menu.classList.toggle("new-menu");
+  changeName.innerHTML = "ATB";
 }
